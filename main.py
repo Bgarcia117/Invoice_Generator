df2a9582d02603082f9b1aee5fced8109ed221fb
Bgarcia117 <bryant.garcia.1771@gmail.com>
2023-07-26 15:22:34 -0500

    Added dates to invoices.

--- main.py
+++ main.py
@@ -8,12 +8,21 @@ filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     # Path(filepath).stem contains the filepath and returns the file name
     filename = Path(filepath).stem
     # Splits the filename and returns a list with both halves
-    invoice_nr = filename.split("-")[0]
+    invoice_nr, date = filename.split("-")[0]
+
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice # {invoice_nr}")
+    pdf.cell(w=50, h=8, txt=f"Invoice # {invoice_nr}", ln=1)
+    
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}")
+
+
+
     pdf.output(f"PDFs/{filename}.pdf")
